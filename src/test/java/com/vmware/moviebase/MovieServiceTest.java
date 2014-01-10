0e8c2ad8c93a9c83d14cd24ba49687b7a34d348e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-01-09 22:18:45 -0600

    added another test for movie service

--- src/test/java/com/vmware/moviebase/MovieServiceTest.java
+++ src/test/java/com/vmware/moviebase/MovieServiceTest.java
@@ -4,19 +4,25 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class MovieServiceTest {
-   
+
 	@Test
 	public void addTwoMovieAndGetTheTotal() {
-		//Subject Under Test
-		MovieService movieService = new MovieService("VMWare Movie Night");  
+		// Subject Under Test
+		MovieService movieService = new MovieService("VMWare Movie Night");
 		movieService.addMovie(new Movie("E.T. - The Extra Terrestrial"));
 		movieService.addMovie(new Movie("Wall-E"));
 		assertEquals(movieService.getCount(), 2);
 	}
-	
+
 	@Test
-	public void addOneMovieAndGetTheTotal() {}
-	
+	public void addOneMovieAndGetTheTotal() {
+		// Subject Under Test
+		MovieService movieService = new MovieService("VMWare Movie Night");
+		movieService.addMovie(new Movie("Wall-E"));
+		assertEquals(movieService.getCount(), 1);
+	}
+
 	@Test
-	public void getTheTotalWithNoMovies() {}
+	public void getTheTotalWithNoMovies() {
+	}
 }
